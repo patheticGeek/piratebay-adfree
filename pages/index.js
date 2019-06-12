0f6274e7a3c18b5abbfad4a00295b4d8637bc410
patheticGeek <shivamnagi14@gmail.com>
2019-06-12 20:18:28 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Index extends Component {
+  state = { site: '', searchTerm: '', searchResults: [], torrentLink: '', torrentDetails: {} };
+  render() {
+    return <div>Hi from nextjs</div>;
+  }
+}
---
+++ server.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const next = require('next');
+const puppeteer = require('puppeteer');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+async function getSitesAvail() {
+  try {
+    var browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    var page = await browser.newPage();
+    await page.goto('https://piratebay-proxylist.se/');
+
+    var searchResults = await page.evaluate(async () => {
+      var list = document.querySelectorAll('table.proxies > tbody > tr');
+      var proxies = [];
+      list.forEach(item => {
+        proxies.push({
+          site: item.querySelectorAll('td')[0].innerText,
+          link: 'https://' + item.querySelectorAll('td')[0].innerText,
+          country: item.querySelectorAll('td')[1].innerText,
+          speed: item.querySelectorAll('td')[2].innerText
+        });
+      });
+
+      return { error: false, proxies };
+    });
+
+    await page.close();
+    await browser.close();
+
+    return searchResults;
+  } catch (err) {
+    console.log(err);
+    return { error: true, message: 'Runtime error occured' };
+  }
+}
+
+async function getSearchResults(site, search) {
+  try {
+    var browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    var page = await browser.newPage();
+    await page.goto(site + '/s/?q=' + search);
+
+    var searchResults = await page.evaluate(async () => {
+      var searchResults = document.querySelector('div#SearchResults');
+      var tableRows = searchResults.querySelectorAll('tr');
+      var results = [];
+
+      tableRows.forEach(item => {
+        if (item.classList.value === 'header') {
+        } else {
+          results.push({
+            name: item.querySelectorAll('td')[1].querySelector('a').innerText,
+            link: item.querySelectorAll('td')[1].querySelector('a').href,
+            seeds: item.querySelectorAll('td')[2].innerText,
+            details: item.querySelectorAll('td')[1].querySelector('font.detDesc').innerText
+          });
+        }
+      });
+      return { error: false, results };
+    });
+
+    await page.close();
+    await browser.close();
+
+    return searchResults;
+  } catch (err) {
+    console.log(err);
+    return { error: true, message: 'Runtime error occured' };
+  }
+}
+
+async function getTorrent(link) {
+  try {
+    var browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    var page = await browser.newPage();
+    await page.goto(link);
+
+    var searchResults = await page.evaluate(async () => {
+      var detailsFrame = document.querySelector('div#detailsframe');
+      var title = detailsFrame.querySelector('div#title').innerText;
+      var uploaded = detailsFrame.querySelectorAll('div#details > .col2 > dd')[0].innerText;
+      var seeds = detailsFrame.querySelectorAll('div#details > .col2 > dd')[2].innerText;
+      var downloadLink = detailsFrame.querySelector('div.download > a').href;
+      var info = detailsFrame.querySelector('div.nfo > pre').innerText;
+      var uploadedBy = detailsFrame.querySelectorAll('div#details > .col2 > dd')[1].innerText;
+
+      return { error: false, torrent: { title, uploaded, uploadedBy, seeds, info, downloadLink } };
+    });
+
+    await page.close();
+    await browser.close();
+
+    return searchResults;
+  } catch (err) {
+    console.log(err);
+    return { error: true, message: 'Runtime error occured' };
+  }
+}
+
+app.prepare().then(() => {
+  const server = express();
+
+  // server.get('/posts/:id', (req, res) => {
+  //   return app.render(req, res, '/posts', { id: req.params.id })
+  // })
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.get('/sitesAvail', async (req, res) => {
+    res.send(await getSitesAvail());
+  });
+
+  server.get('/search', async (req, res) => {
+    let search = req.query.search;
+    let site = req.query.site;
+    if (search === '' || !search || site === '' || !site) {
+      res.send({ error: true, errorMessage: 'Site or search cannot be empty' });
+    } else {
+      res.send(await getSearchResults(site, search));
+    }
+  });
+
+  server.get('/torrent', async (req, res) => {
+    let link = req.query.link;
+    if (link === '' || !link) {
+      res.send({ error: true, errorMessage: 'Link cannot be empty' });
+    } else {
+      res.send(await getTorrent(link));
+    }
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
